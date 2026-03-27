cab169852e6a90eae0ccb32f22d0f3199dd8745f
Prathmesh1704 <bapteprathamesh@gmail.com>
2026-03-27 20:52:36 +0530

    Added new ArrayQuestion - Findduplicate

--- Arrays/FindDuplicate.java
+++ Arrays/FindDuplicate.java
@@ -23,4 +23,103 @@ class FindDuplicate {
 */
 
 
-/* Approach 2 sorting */
+/* Approach 2 sorting 
+
+import java.util.*;
+
+class FindDuplicate {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
+
+*/
+
+/* Arrpoach 3 HashSet 
+
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+      Set<Integer> set = new HashSet();
+      for(int i=0;i<nums.length;i++){
+        if(set.contains(nums[i])){
+             return nums[i];
+           
+        }
+        else set.add(nums[i]);
+      }
+        
+        return -1;
+    }
+}
+
+
+
+*/
+
+/* Approach 3 
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+      Set<Integer> set = new HashSet();
+      for(int i=0;i<nums.length;i++){
+        if(set.contains(nums[i])){
+             return nums[i];
+           
+        }
+        else set.add(nums[i]);
+      }
+        
+        return -1;
+    }
+}
+
+
+*/
+
+
+/* Approach 5 Floyds hare and tortoise method
+
+
+ class Solution {
+    public int findDuplicate(int[] nums) {
+      int slow =nums[0];
+      int fast = nums[0];
+
+      while(true){
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        if(slow==fast){
+            break;
+        }
+
+      }
+      int slow2=nums[0];
+      while(slow2!= slow){
+        slow=nums[slow];
+        slow2=nums[slow2];
+      } return slow;
+    
+        
+      }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
