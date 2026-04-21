543c9c83772eeff70fa66cf94ffbb2588d922910
Prathmesh1704 <bapteprathamesh@gmail.com>
2026-04-21 14:31:28 +0530

    Added new String question Validpalindrome

--- String/ValidPalindrome.java
+++ String/ValidPalindrome.java
@@ -15,3 +15,28 @@ public class ValidPalindrome {
     }
 }
     
+
+//this regex method  increases space while creating a new string 
+// hence instead you can use simple code like
+/*
+int left = 0, right = s.length() - 1;
+
+while(left < right){
+    while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+        left++;
+    }
+    while(left < right && !Character.isLetterOrDigit(s.charAt(right))){
+        right--;
+    }
+
+    if(Character.toLowerCase(s.charAt(left)) != 
+       Character.toLowerCase(s.charAt(right))){
+        return false;
+    }
+
+    left++;
+    right--;
+}
+return true;
+
+*/
