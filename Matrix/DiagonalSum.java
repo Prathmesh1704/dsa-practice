00c22028214650593332235caf7890c1ac236cd7
Prathmesh1704 <bapteprathamesh@gmail.com>
2026-05-26 12:20:12 +0530

    Added new ArrayQuestion

---
+++ Matrix/DiagonalSum.java
@@ -0,0 +1,88 @@
+package Matrix;
+
+
+/*Primary Diagonal*/
+import java.util.Scanner;
+
+public class DiagonalSum {
+
+     public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter no of rows:");
+        
+        int rows= sc.nextInt();
+        System.out.print("Enter no of Cols:");
+
+        int cols = sc.nextInt();
+
+        int [][] arr = new int[rows][cols];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                arr[i][j] = sc.nextInt();
+            }
+            System.out.println();
+        }
+
+        int sum =0;
+        for(int i =0;i<rows;i++){
+
+            for(int j=0;j<cols;j++){
+                if(i==j){
+                    sum+=arr[i][j];
+                }
+            }
+        }
+        System.out.println(sum);
+    
+}
+}
+
+
+/*
+
+Secondary Diagonal
+
+
+package Matrix;
+
+import java.util.Scanner;
+
+public class DiagonalSum {
+
+     public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter no of rows:");
+        
+        int rows= sc.nextInt();
+        System.out.print("Enter no of Cols:");
+
+        int cols = sc.nextInt();
+
+        int [][] arr = new int[rows][cols];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                arr[i][j] = sc.nextInt();
+            }
+            System.out.println();
+        }
+
+        int sum =0;
+        for(int i =0;i<rows;i++){
+
+            for(int j=0;j<cols;j++){
+                if(i+j==n-1){
+                    sum+=arr[i][j];
+                }
+            }
+        }
+        System.out.println(sum);
+    
+}
+}
+
+
+
+
+*/
+
+
